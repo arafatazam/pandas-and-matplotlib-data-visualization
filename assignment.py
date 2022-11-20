0730cf3409a0e6a1d141345b33febc046089bcd4
Arafat Azam <arafatazam17@gmail.com>
2022-11-20 18:28:15 +0000

    Data cleaning method

--- assignment.py
+++ assignment.py
@@ -8,13 +8,25 @@ Created on Wed Nov 16 20:24:59 2022
 
 import pandas as pd
 
-def clean_data(df:pd.DataFrame):
-    pass
-    
+
+def clean_data(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Discard NaN values, take only top lavel ethnic groups
+    and cast to int64
+    """
+    df = df.dropna()
+    df = df.filter(regex='All|Total', axis="index")
+    for col in df.columns:
+        df[col] = df[col].str.replace(',', '')
+        df[col] = pd.to_numeric(df[col])
+    return df
+
 
 def main():
     data = pd.read_csv('age-groups.csv', index_col=0)
-    print(data.head())
-    
-if __name__=="__main__":
+    data = clean_data(data)
+    print(data.iloc[1:]['All ages'].sum())
+
+
+if __name__ == "__main__":
     main()
